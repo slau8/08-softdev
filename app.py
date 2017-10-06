4549333eb493f79d47b0a6491fd045e7497e84e9
Shannon Lau <shannonlau00@gmail.com>
2017-10-06 13:52:59 -0400

    template done

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 '''
-Shannon Lau and Taylor Wong
+Shannon Lau and Tiffany Moi
 SoftDev1 Period 7
-HW #07: Do I Know You?
-2017-10-4
+HW #08: Do I Know You?
+2017-10-6
 '''
 
 from flask import Flask, render_template, redirect, url_for, session, request
@@ -14,35 +14,41 @@ app = Flask(__name__)
 #encrypts cookie values
 app.secret_key = os.urandom(32)
 
-accounts = {'hello':'world'}
+ACCOUNTS = {'tiff':'coder', 'shanlau':'baller'}
 
 # Landing page
 @app.route("/")
 def hello_world():
-    # If not logged out, render welcome page
+    # If not logged out, redirect to welcome page
     # Else, render login page
     if 'uname' in session.keys():
-        return render_template('welcome.html', USERNAME = session['uname'])
+        return redirect(url_for('welcome'))
     else:
-        return render_template('login.html', ERROR = "")
+        return render_template('login.html', message = "")
 
-# Renders welcome or login page (with error)
-@app.route('/welcome')
-def welcome():
+# Render login page (with error) or redirect to welcome page
+@app.route('/login')
+def login():
     form_dict = request.args
     uname = form_dict['uname']
     password = form_dict['password']
     # Check for existing username
-    if uname in accounts.keys():
-        # Check for valid password
-        if (accounts[uname] == password):
-            # Add username data to session
-            session['uname'] = uname
-            return render_template('welcome.html', USERNAME = uname)
-        else:
-            return render_template('login.html', ERROR = 'Incorrect password.')
-    else:
-        return render_template('login.html', ERROR = 'Incorrect username.')
+    if uname not in ACCOUNTS:
+        return render_template('login.html', message = 'Incorrect username.')
+    # Check for valid password
+    if ACCOUNTS[uname] != password:
+        return render_template('login.html', message = 'Incorrect password.')
+    # Add username data to session
+    session['uname'] = uname
+    # INSERT CODE HERE TO REDIRECT WELCOME
+
+
+# Render welcome page
+@app.route('/welcome')
+def welcome():
+    uname = session['uname']
+    # INSERT CODE HERE TO RENDER TEMPLATE
+    # Note: in the template, the variable for username is 'username'
 
 # Log out procedure
 @app.route('/logout')
@@ -50,7 +56,7 @@ def logout():
     # Remove username data from session
     if 'uname' in session:
         session.pop('uname')
-    return render_template('login.html', ERROR = "")
+    return redirect(url_for('hello_world'))
 
 if __name__ == "__main__":
     app.debug = True
